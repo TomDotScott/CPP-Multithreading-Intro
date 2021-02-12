bf1504f327ee7205d04a049bcb1cd1fef2d9472e
TomDotScott <tom.scott45456@gmail.com>
2021-02-12 14:08:27 +0000

    Enemies fighting across threads

--- Threads Intro/main.cpp
+++ Threads Intro/main.cpp
@@ -2,34 +2,96 @@
 #include <thread>
 #include <vector>
 
-class Loop
+
+class Actor
 {
 public:
-	void Run()
+	Actor(const int hp, const int attackPoints, const int defensePoints, std::string name) :
+		m_hp(hp),
+		m_attack(attackPoints),
+		m_defense(defensePoints),
+		m_name(std::move(name)),
+		m_opponent(nullptr)
 	{
-		while(!hasToStop)
-		{
-			std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-		}
-		std::cout << "Finishing the loop" << std::endl;
 	}
 
-	bool hasToStop{false};
+	virtual ~Actor() = default;
+
+	void Attack() const
+	{
+		if (m_opponent)
+		{
+			std::cout << m_name << " attacks " << m_opponent->m_name << std::endl;
+			m_opponent->m_hp -= (m_attack - m_opponent->m_defense);
+			std::cout << m_opponent->m_name << " HP: " << m_opponent->GetHP() << std::endl;
+		}
+	}
+
+	virtual void Run() = 0;
+
+	int GetHP() const { return m_hp; }
+
+	void SetOpponent(Actor* other)
+	{
+		m_opponent = other;
+	}
+
+protected:
+	int m_hp;
+	int m_attack;
+	int m_defense;
+
+private:
+	const std::string m_name;
+	Actor* m_opponent;
+
+};
+
+
+class RolforTheBarbarian : public Actor
+{
+public:
+	RolforTheBarbarian(const int hp, const int attack, const int defense) : Actor(hp, attack, defense, "Rolfor") {}
+
+	void Run() override
+	{
+		std::this_thread::sleep_for(std::chrono::milliseconds(rand() % 1000));
+		Attack();
+	}
+};
+
+class GuardDanTheGnome : public Actor
+{
+public:
+	GuardDanTheGnome(const int hp, const int attack, const int defense) : Actor(hp, attack, defense, "Dan") {}
+
+	void Run() override
+	{
+		std::this_thread::sleep_for(std::chrono::milliseconds(rand() % 2000));
+		Attack();
+	}
 };
 
 int main()
 {
-	srand(time(NULL));
-	Loop loop;
-	std::thread t1(&Loop::Run, &loop);
+	srand(static_cast<unsigned>(time(nullptr)));
 
-	std::thread t2([&loop]()   // this is a lambda... an anonymous function
+	GuardDanTheGnome dan(150, 35, 55);
+	RolforTheBarbarian rolf(100, 50, 30);
+
+	dan.SetOpponent(&rolf);
+	rolf.SetOpponent(&dan);
+	
+	while (dan.GetHP() > 0 && rolf.GetHP() > 0)
 	{
-		std::this_thread::sleep_for(std::chrono::milliseconds(rand() % 1000));
-		loop.hasToStop = true;
-	});
+		std::thread gd(&GuardDanTheGnome::Run, &dan);
+		//std::thread rf(&RolforTheBarbarian::Run, &rolf);
 
-	t2.join();
-	t1.join();
+		//rf.join();
+		gd.join();
+	}
+
+	std::cout << (dan.GetHP() > 0 ? "Dan wins" : "Rolfor wins") << std::endl;
+
 	return 0;
 }
